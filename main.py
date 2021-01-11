d68d1d65b75c85abc441ab3189a32cce82b24ded
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-01-11 16:22:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import discord
+import discord #On importe la librairie
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="t", description=None)
+bot = commands.Bot(command_prefix="t", description=None) #On définit le préfixe du bot
 
 @bot.event
 async def on_ready():
-    print("Le bot est prêt")
+    print("Le bot est prêt") #On lance le bot
 
 @bot.command()
-async def close(ctx):
+async def close(ctx): #Commande pour éteindre le bot
     await ctx.send("Le bot va s'éteindre")
     await bot.close()
 
-bot.run("ton token")
+bot.run(config.token) #Le bot va se lancer grâce au token
